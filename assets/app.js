b2ac56db25c08bfaf2ef06864e861cd93124a9a2
melindaa <166626146+melindaland@users.noreply.github.com>
2025-04-18 15:26:50 +0200

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './styles/app.css';
+import './styles/accueil.css';
+import './styles/admin.css';
+import './styles/connexion.css';
+import './styles/forum.css';
+import './styles/marqueur.css';
+import './styles/parcours.css';
+import './styles/carte.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ assets/dist/app.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+require("./bootstrap.js");
+
+require("./styles/app.css");
+
+require("./styles/accueil.css");
+
+require("./styles/admin.css");
+
+require("./styles/connexion.css");
+
+require("./styles/forum.css");
+
+require("./styles/marqueur.css");
+
+require("./styles/parcours.css");
+
+require("./styles/carte.css");
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+//# sourceMappingURL=app.dev.js.map
